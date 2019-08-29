82f9978f6daeef9d2cf02d90ed320c9b1508ae90
mrkazawa <yustus.oktian@gmail.com>
2019-08-29 12:57:42 +0900

    add sending auth payload to contract from owner

--- actors/owner.js
+++ actors/owner.js
@@ -1,13 +1,16 @@
 const EthCrypto = require('eth-crypto');
 const fs = require('fs');
 const Web3 = require('web3');
+
 const tools = require('./actor_tools');
 
 // the path to the preloaded info from ganache network
 const ownerPath = './ganache_files/owner.json';
 const ISPPath = './ganache_files/isp.json';
+const gatewayPath = './ganache_files/gateway.json';
 const contractPath = './ganache_files/contract.json';
 const contractABIPath = '../build/contracts/RegistryContract.json';
+
 // connect to ganache network
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
@@ -25,6 +28,25 @@ async function main() {
     // we hardcoded the process by reading ISP credentials file
     data = fs.readFileSync(ISPPath, 'utf8');
     const ISP = JSON.parse(data);
+    // owner also knows the credentials of the gateway
+    data = fs.readFileSync(gatewayPath, 'utf8');
+    const GW = JSON.parse(data);
+    // parsing the local ABI from truffle
+    // in live network, the ABI can be queried from etherscan.io
+    data = fs.readFileSync(contractABIPath, 'utf8');
+    const obj = JSON.parse(data);
+    // instantiate by address
+    data = fs.readFileSync(contractPath, 'utf8');
+    const contract = JSON.parse(data);
+
+    // checksuming all address
+    const ownerAddres = web3.utils.toChecksumAddress(owner.address);
+    const ISPAddress = web3.utils.toChecksumAddress(ISP.address);
+    const GWAddress = web3.utils.toChecksumAddress(GW.address);
+    const RCAddress = web3.utils.toChecksumAddress(contract.address);
+
+    // creating RegistryContract from deployed contract at the given address
+    const RC = new web3.eth.Contract(obj.abi, RCAddress);
 
     // preparing payload
     const auth = {
@@ -43,17 +65,16 @@ async function main() {
     const signature = EthCrypto.sign(owner.privateKey, payloadHash);
 
     // sending transaction to register payload to the smart contract
-    
-    // parsing the local ABI from truffle
-    // in live network, the ABI can be queried from etherscan.io
-    data = fs.readFileSync(contractABIPath, 'utf8');
-    const obj = JSON.parse(data);
-    // instantiate by address
-    data = fs.readFileSync(contractPath, 'utf8');
-    const contract = JSON.parse(data);
-    var RegistryContract = new web3.eth.Contract(obj.abi, contract.address);
-    
-    RegistryContract.methods.isTrustedGateway(ISP.address).call({from: owner.address}).then(console.log);
+    let tx = await RC.methods.storeAuthNPayload(payloadHash, GWAddress, ISPAddress).send({from: ownerAddres, gas: 1000000});
+    if (typeof tx.events.NewPayloadAdded !== 'undefined') {
+        const event = tx.events.NewPayloadAdded;
+        if (event.returnValues['sender'] == ownerAddres && 
+        event.returnValues['IPFSHash'] == payloadHash) {
+            console.log('transaction received by contract!');
+        }
+    }
+
+    //RC.methods.isTrustedGateway(ISP.address).send({from: owner.address}).then(console.log);
 
     // sending authentication payload to the ISP
 }
