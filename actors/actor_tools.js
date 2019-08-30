769ad474e661bf535f80b9a903263161fefafb45
mrkazawa <yustus.oktian@gmail.com>
2019-08-30 16:54:23 +0900

    refactor the owner code

--- actors/actor_tools.js
+++ actors/actor_tools.js
@@ -1,15 +1,35 @@
+const EthCrypto = require('eth-crypto');
 const crypto = require('crypto');
+const fs = require('fs');
+const Web3 = require('web3');
 
+// the path to the preloaded info from ganache network
+const ownerPath = './ganache_files/owner.json';
+const ISPPath = './ganache_files/isp.json';
+const gatewayPath = './ganache_files/gateway.json';
+const contractPath = './ganache_files/contract.json';
+const contractABIPath = '../build/contracts/RegistryContract.json';
+
+// the endpoints
+const ISPAuthEndpoint = 'http://localhost:3000/authenticate';
+const vendorAuthEndpoint = 'http://localhost:4000/authenticate';
+
+// for symmetric encryption and decryption
 const algorithm = 'aes256';
 const inputEncoding = 'utf8';
 const outputEncoding = 'hex';
 
-module.exports = {
+// connect to ganache network
+// run the following command to activate ganache
+// ganache-cli -m dongseo
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+var self = module.exports = {
     /**
      * Generate random string in Base64 form.
-     * @param {*} len the length of the string
+     * @param {int} len     the length of the string.
      */
-    randomValueBase64(len) {
+    randomValueBase64: function (len) {
         return crypto
           .randomBytes(Math.ceil((len * 3) / 4))
           .toString('base64') // convert to base64 format
@@ -17,14 +37,144 @@ module.exports = {
           .replace(/\+/g, '0') // replace '+' with '0'
           .replace(/\//g, '0') // replace '/' with '0'
     },
-
+    /**
+     * Read JSON file and return the contents of the file in object.
+     * Also convert the address of Ethereum (if any) to checksum format.
+     * @param {string} path     path to the JSON file.
+     */
+    readFile: function (path) {
+        let data = fs.readFileSync(path, 'utf8');
+        return JSON.parse(data);
+    },
+    /**
+     * Get owner private key from ganache configuration.
+     */
+    getOwnerPrivateKey: function () {
+        let obj = self.readFile(ownerPath);
+        return obj.privateKey;
+    },
+    /**
+     * Get owner public key from ganache configuration.
+     */
+    getOwnerPublicKey: function () {
+        let obj = self.readFile(ownerPath);
+        return EthCrypto.publicKeyByPrivateKey(obj.privateKey);
+    },
+    /**
+     * Get owner address from ganache configuration.
+     */
+    getOwnerAddress: function() {
+        let obj = self.readFile(ownerPath);
+        return web3.utils.toChecksumAddress(obj.address);
+    },
+    /**
+     * Get ISP private key from ganache configuration.
+     */
+    getISPPrivateKey: function () {
+        let obj = self.readFile(ISPPath);
+        return obj.privateKey;
+    },
+    /**
+     * Get ISP public key from ganache configuration.
+     */
+    getISPPublicKey: function () {
+        let obj = self.readFile(ISPPath);
+        return EthCrypto.publicKeyByPrivateKey(obj.privateKey);
+    },
+    /**
+     * Get ISP address from ganache configuration.
+     */
+    getISPAddress: function() {
+        let obj = self.readFile(ISPPath);
+        return web3.utils.toChecksumAddress(obj.address);
+    },
+    /**
+     * Get gateway private key from ganache configuration.
+     */
+    getGatewayPrivateKey: function () {
+        let obj = self.readFile(gatewayPath);
+        return obj.privateKey;
+    },
+    /**
+     * Get gateway public key from ganache configuration.
+     */
+    getGatewayPublicKey: function () {
+        let obj = self.readFile(gatewayPath);
+        return EthCrypto.publicKeyByPrivateKey(obj.privateKey);
+    },
+    /**
+     * Get gateway address from ganache configuration.
+     */
+    getGatewayAddress: function() {
+        let obj = self.readFile(gatewayPath);
+        return web3.utils.toChecksumAddress(obj.address);
+    },
+    /**
+     * Get contract address from ganache after 'truffle deploy'.
+     */
+    getContractAddress: function () {
+        let obj = self.readFile(contractPath);
+        return web3.utils.toChecksumAddress(obj.address);
+    },
+    /**
+     * Parsing the local contract ABI from truffle.
+     * in live network, the ABI can be queried from etherscan.io
+     */
+    getContractABI: function () {
+        let obj = self.readFile(contractABIPath);
+        return obj.abi;
+    },
+    /**
+     * Get the endpoint URL to authenticate the gateway.
+     */
+    getISPAuthEndpoint: function () {
+        return ISPAuthEndpoint;
+    },
+    /**
+     * Get the endpoint URL to authenticate the device.
+     */
+    getVendorAuthEndpoint: function () {
+        return vendorAuthEndpoint;
+    },
+    /**
+     * Construct a web3 object of the smart contract.
+     * @param {string} abi       the ABI of the contract.
+     * @param {string} address   the address of the deployed contract.
+     */
+    constructSmartContract: function (abi, address) {
+        return new web3.eth.Contract(abi, address);
+    },
+    /**
+     * Encrypt the payload with destination public key.
+     * Hash the encrypted payload.
+     * Then, sign the message with the source private key.
+     * @param {string} payload          Payload to be encrypted and signed.
+     * @param {hex} destPublicKey       Key used to encrypt.
+     * @param {hex} sourcePrivateKey    Key used to sign.
+     */
+    encryptAndSignPayload: async function (payload, destPublicKey, sourcePrivateKey) {
+        const authEncrypted = await EthCrypto.encryptWithPublicKey(destPublicKey, payload);
+        const authEncryptedPayload = EthCrypto.cipher.stringify(authEncrypted);
+        const authPayloadHash = EthCrypto.hash.keccak256(authEncryptedPayload);
+        const authSignature = EthCrypto.sign(sourcePrivateKey, authPayloadHash);
+        return [authPayloadHash, authSignature, authEncryptedPayload];
+    },
+    /**
+     * Encrypt the message using symmetric encryption.
+     * @param {hex} key         Key used to encrypt.
+     * @param {string} data     Payload to be encrypted.
+     */
     encryptSymmetrically(key, data) {
         let cipher = crypto.createCipher(algorithm, key);
         let ciphered = cipher.update(data, inputEncoding, outputEncoding);
         ciphered += cipher.final(outputEncoding);
         return ciphered;
     },
-
+    /**
+     * Decrypt the message using symmetric encryption.
+     * @param {hex} key             Key used to decrypt.
+     * @param {string} ciphered     Payload to be decrypted.
+     */
     decryptSymmetrically(key, ciphered) {
         let decipher = crypto.createDecipher(algorithm, key);
         let deciphered = decipher.update(ciphered, outputEncoding, inputEncoding);
--- actors/owner.js
+++ actors/owner.js
@@ -1,99 +1,65 @@
-const EthCrypto = require('eth-crypto');
-const fs = require('fs');
 const rp = require('request-promise-native');
-const Web3 = require('web3');
-
 const tools = require('./actor_tools');
 
-// the path to the preloaded info from ganache network
-const ownerPath = './ganache_files/owner.json';
-const ISPPath = './ganache_files/isp.json';
-const gatewayPath = './ganache_files/gateway.json';
-const contractPath = './ganache_files/contract.json';
-const contractABIPath = '../build/contracts/RegistryContract.json';
-
-// connect to ganache network
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
 async function main() {
-    // example of authentication payload for the ISP
-    const routerIP = '200.100.10.10';
-    const username = 'john';
-    const password = 'fish';
-    const nonce = tools.randomValueBase64(32);
-
-    // get owner credentials
-    let data = fs.readFileSync(ownerPath, 'utf8');
-    const owner = JSON.parse(data);
-    // we assume the author knows the ISP public key
-    // we hardcoded the process by reading ISP credentials file
-    data = fs.readFileSync(ISPPath, 'utf8');
-    const ISP = JSON.parse(data);
-    // owner also knows the credentials of the gateway
-    data = fs.readFileSync(gatewayPath, 'utf8');
-    const GW = JSON.parse(data);
-    // parsing the local ABI from truffle
-    // in live network, the ABI can be queried from etherscan.io
-    data = fs.readFileSync(contractABIPath, 'utf8');
-    const obj = JSON.parse(data);
-    // instantiate by address
-    data = fs.readFileSync(contractPath, 'utf8');
-    const contract = JSON.parse(data);
-
-    // checksuming all address
-    const ownerAddres = web3.utils.toChecksumAddress(owner.address);
-    const ISPAddress = web3.utils.toChecksumAddress(ISP.address);
-    const GWAddress = web3.utils.toChecksumAddress(GW.address);
-    const RCAddress = web3.utils.toChecksumAddress(contract.address);
+    // setup parameters that are known by the owner.
+    const ownerAddress = tools.getOwnerAddress();
+    const ownerPrivateKey = tools.getOwnerPrivateKey();
+    const ISPPublicKey = tools.getISPPublicKey();
+    const ISPAddress = tools.getISPAddress();
+    const gatewayAddress = tools.getGatewayAddress();
 
     // creating RegistryContract from deployed contract at the given address
-    const RC = new web3.eth.Contract(obj.abi, RCAddress);
+    const RC = tools.constructSmartContract(tools.getContractABI(), tools.getContractAddress());
 
-    // preparing payload
+    // example of authentication payload for the ISP
     const auth = {
-        routerIP: routerIP,
-        username: username,
-        password: password,
-        nonce: nonce
+        routerIP: '200.100.10.10',
+        username: 'john',
+        password: 'fish',
+        nonce: tools.randomValueBase64(32)
     };
     const authPayload = JSON.stringify(auth);
-    // encrypting using ISP public key
-    const ISPPublicKey = EthCrypto.publicKeyByPrivateKey(ISP.privateKey);
-    const authEncrypted = await EthCrypto.encryptWithPublicKey(ISPPublicKey, authPayload);
-    const authEncryptedPayload = EthCrypto.cipher.stringify(authEncrypted);
-    // signing using OWNER private key
-    const authPayloadHash = EthCrypto.hash.keccak256(authEncryptedPayload);
-    const authSignature = EthCrypto.sign(owner.privateKey, authPayloadHash);
+    const [authPayloadHash, authSignature, authEncryptedPayload] = await tools.encryptAndSignPayload(authPayload, ISPPublicKey, ownerPrivateKey);
 
     // sending transaction to register payload to the smart contract
-    let tx = await RC.methods.storeAuthNPayload(authPayloadHash, GWAddress, ISPAddress).send({
-        from: ownerAddres,
+    let tx = await RC.methods.storeAuthNPayload(authPayloadHash, gatewayAddress, ISPAddress).send({
+        from: ownerAddress,
         gas: 1000000
     });
     if (typeof tx.events.NewPayloadAdded !== 'undefined') {
         const event = tx.events.NewPayloadAdded;
-        if (event.returnValues['sender'] == ownerAddres &&
+        if (event.returnValues['sender'] == ownerAddress &&
             event.returnValues['IPFSHash'] == authPayloadHash) {
             console.log('transaction received by contract!');
-        }
-    }
 
-    const authEndpoint = 'http://localhost:3000/authenticate';
-    let options = {
-        method: 'POST',
-        uri: authEndpoint,
-        body: {
-            authPayload: authEncryptedPayload,
-            authSignature: authSignature
-        },
-        json: true // Automatically stringifies the body to JSON
-    };
-    // sending authentication payload to the ISP
-    rp(options).then(function (parsedBody) {
-        console.log('sent');
-    }).catch(function (err) {
-        console.log(err);
-    });
+            // sending authentication payload to the ISP
+            let options = {
+                method: 'POST',
+                uri: tools.getISPAuthEndpoint(),
+                body: {
+                    authPayload: authEncryptedPayload,
+                    authSignature: authSignature
+                },
+                resolveWithFullResponse: true,
+                json: true // Automatically stringifies the body to JSON
+            };
+            rp(options).then(function (response) {
+                if (response.statusCode == '200') {
+                    console.log('success!');
+                } else {
+                    console.log('error!');
+                }
+                console.log(response.body);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        } else {
+            console.log('received wrong event!');
+        }
+    } else {
+        console.log('transaction failed!');
+    }
 }
 
 main();
